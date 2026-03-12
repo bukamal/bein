9ba20855183dd52e70cc0cb83c6fb5207071f17a
bukamal <79067825+bukamal@users.noreply.github.com>
2026-03-13 01:50:24 +0300

    Delete frontend directory

--- frontend/components/UnreadBadge.js
+++
@@ -1,9 +0,0 @@
-export default function UnreadBadge({ unreadCount }) {
-    return (
-        unreadCount > 0 && (
-            <span className="ml-2 bg-red-500 text-white text-xs font-bold px-2 py-1 rounded-full">
-                {unreadCount}
-            </span>
-        )
-    );
-}
